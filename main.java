83cfa1e8ae0be05b75ea545ed61ba1e1a2413856
delfinx <simone.orlando.007@gmail.com>
2025-05-03 20:43:09 +0200

    PARTE BASE COMPLETATA

--- main.java
+++ main.java
@@ -1,24 +1,74 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+
 public class main
 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
+    //PUNTO 1
     ArrayList<String> nomi = new ArrayList<>();
-    nomi.add("Simone");
-    nomi.add("Giorgi");
-    nomi.add("Franci");
-    nomi.add("Isra");
-    nomi.add("Bencex");
+    nomi.add("simone");
+    nomi.add("giorgi");
+    nomi.add("franci");
+    nomi.add("isra");
+    nomi.add("bencex");
     for(String nome: nomi){
         System.out.println(nome);
     }
 
+    //PUNTO 2
     grandezzaArreyList(nomi);
     
+    //PUNTO 3
+    ArrayList<Integer> numeri = new ArrayList<>();
+    numeri.add(1);
+    numeri.add(2);
+    numeri.add(3);
+    numeri.add(4);
+    numeri.add(5);
+
+    int somma = 0;
+    for(int numero: numeri){
+        somma += numero;
+    }
+    System.out.println(somma);
+
+    //PUNTO 4
+    System.out.println("Inserisci nome di persona:");
+    String nomeInserito = input.nextLine().toLowerCase();
+
+    int grandezzaLista = nomi.size();
+    int count = 0;
+    for(String parola: nomi){
+        if(parola.equals(nomeInserito)){
+            count++;
+        }
+    }
+
+    if(count > 0){
+        System.out.println(nomeInserito + " è presente nella lista data");
+    }
+
+    else{
+        System.out.println(nomeInserito + " non è presente nella lista data");
+    }
+
+    //PUNTO 5
+    System.out.println("Dalla lista data precedentemente, rimuovi un nome");
+    String nomeRimosso = input.nextLine().toLowerCase();
+    for(String parola: nomi){
+        if(nomeRimosso.equals(parola)){
+            nomi.remove(parola);
+        }
+    }
+
+    for(String nome: nomi){
+        System.out.println(nome);
+    }
     
+
     }
     public static void grandezzaArreyList(ArrayList<String> nomi){
         int grandezza = nomi.size();
